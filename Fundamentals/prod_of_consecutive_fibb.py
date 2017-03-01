3bc1b41692a6214d3b5f10414927e4b5cbd3c3c7
schottkey <schottkey@gmail.com>
2017-03-01 01:29:04 +0000

    feat: Added product of consecutive fibonacci numbers solution
    Problem URL: https://www.codewars.com/kata/product-of-consecutive-fib-numbers

---
+++ Fundamentals/prod_of_consecutive_fibb.py
@@ -0,0 +1,56 @@
+"""
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n)
+and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prodyou will return
+
+[F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+"""
+
+import functools
+
+
+@functools.lru_cache(None)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+def product_fib(N):
+    f, s = 0, 1
+    fibf, fibs = fib(f), fib(s)
+    while fibf * fibs < N:
+        f += 1
+        s += 1
+        fibf, fibs = fib(f), fib(s)
+    return [fibf, fibs, fibf * fibs == N]
+
+
+# alternative solution
+def product_fib2(N):
+    f, s = 0, 1
+    while f * s < N:
+        f, s = s, f + s
+    return [f, s, N == f * s]
+
+
+if __name__ == '__main__':
+    assert(product_fib(4895) == [55, 89, True])
+    assert(product_fib(5895) == [89, 144, False])
